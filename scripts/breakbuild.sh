5550532b4268edd58d56d81a1f592bf2155c0dde
VL <viacheslav.lisianskii@jpmchase.com>
2019-08-29 11:39:25 -0400

    some_id

--- scripts/breakbuild.sh
+++ scripts/breakbuild.sh
@@ -7,8 +7,9 @@ if [ -z "SONAR_PROJECT_KEY" ]; then
 fi
 echo "Using SonarCloud project key ${SONAR_PROJECT_KEY}"
 
-activities_response=$(curl -s -u "${SONAR_TOKEN}": "${SONAR_INSTANCE}"/api/ce/activity?component=${SONAR_PROJECT_KEY}&type=REPORT | jq -r '.tasks[0] .id')
-echo "activities_response ${activities_response}"
+activities_response=$(curl -s -u "${SONAR_TOKEN}": "${SONAR_INSTANCE}"/api/ce/activity?component=${SONAR_PROJECT_KEY}&type=REPORT)
+some_id=$(${activities_response} | jq -r '.tasks[0]')
+echo "some_id ${some_id}"
 
 ce_task_id=$(curl -s -u "${SONAR_TOKEN}": "${SONAR_INSTANCE}"/api/ce/activity?component=${SONAR_PROJECT_KEY}&type=REPORT | jq -r '.tasks')
 echo "ce_task_id ${ce_task_id}"
