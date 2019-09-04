9b3238ea12f194fd9310cbe6d93aaf769e0db2b2
VL <viacheslav.lisianskii@jpmchase.com>
2019-09-04 11:17:56 -0400

    removed snapshotrelease.sh

--- scripts/snapshotRelease.sh
+++
@@ -1,8 +0,0 @@
-echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
-echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-
-# get project version
-export project_version=$(mvn help:evaluate -N -Dexpression=project.version|grep -v '\[')
-
-# build then deploy SNAPSHOT to OSSRH
-mvn --settings .maven.xml -B -U clean deploy -DskipTests=true -Prelease
